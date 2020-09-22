ea9a4e114ee25b0de5aabb91d2792145137e2183
区澄毅 <1027017939@qq.com>
2020-09-22 14:37:38 +0800

    idea工程

---
+++ untitled/src/main/java/com/Test/Manualchapter.java
@@ -0,0 +1,12 @@
+package com.Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Manualchapter {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Manualchapter.class);
+    }
+}
